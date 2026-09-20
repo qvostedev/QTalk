0fc5d2737d6c29e6cad68de737b0db12dd7699c1
qvoste <qvostedev@gmail.com>
2026-09-20 23:31:31 +0700

    Fixed Android startup with FakeVoipEngine

--- androidApp/src/main/kotlin/com/qvoste/qtalk/MainActivity.kt
+++ androidApp/src/main/kotlin/com/qvoste/qtalk/MainActivity.kt
@@ -5,8 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 import com.qvoste.qtalk.app.App
+import com.qvoste.qtalk.voip.FakeVoipEngine
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +16,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            App()
+            App(remember { FakeVoipEngine() })
         }
     }
 }
@@ -22,5 +24,5 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun AppAndroidPreview() {
-    App()
+    App(remember { FakeVoipEngine() })
 }
